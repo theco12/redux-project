343f33ceb9bd45435b8ecfdcc949adb3cd299d6f
theco12 <107103821+theco12@users.noreply.github.com>
2022-11-07 13:39:22 +0900

    update

---
+++ shop/src/layout/footer.js
@@ -0,0 +1,7 @@
+import footer from "./layout/footer";
+
+const Footer = () => {
+  return <div>Footer입니다.</div>;
+};
+
+export default Footer;
---
+++ shop/src/layout/header.js
@@ -0,0 +1,7 @@
+import Header from "./layout/header";
+
+const Header = () => {
+  return <div>안녕하세요</div>;
+};
+
+export default Header;
