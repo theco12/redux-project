c5a973075a6ccd4fe7f1b1f40a6bd55be52c82be
theco12 <107103821+theco12@users.noreply.github.com>
2022-11-22 15:09:29 +0900

    update

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import AppLayout from "../components/applayout";
+
+const Home = () => {
+  return (
+    <div>
+      <AppLayout>
+        <div>hello native</div>
+      </AppLayout>
+    </div>
+  );
+};
+
+export default Home;
